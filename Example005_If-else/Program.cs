e8e4ff8e11c2f9c053db9f3b8fbbde261a196bed
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-08 16:55:03 +0300

    программа приветствия любимчика

---
+++ Example005_If-else/Program.cs
@@ -0,0 +1,12 @@
+﻿// приветствие любимчика
+Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "nataly")
+{
+    Console.WriteLine ("Ура, это Nataly!!!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
